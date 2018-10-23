9cac426e9dbced36469316ee0492d791e79a383c
josephtan <tantjernbit@gmail.com>
2018-10-23 22:51:36 +1100

    import PlaySound and DrawKeyboard components

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import "../css/App.scss";
+import PlaySound from "./components/playsound";
+import DrawKeyboard from "./components/drawkeyboard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+            <h1>React Piano</h1>
+        </header>
+        <main className="App-main">
+          <DrawKeyboard/>
+          <PlaySound/>
+        </main>
+        <footer className="App-footer">
+            <p>React Piano</p>
+        </footer>
       </div>
     );
   }
