25d164e61a6e30130cb5f9c51112a5220b3c7b0f
josephtan <tantjernbit@gmail.com>
2018-10-23 22:49:58 +1100

    add axios settings

--- src/axiosOptions.js
+++ src/axiosOptions.js
@@ -1,3 +1,14 @@
 /**
  * Created by Joseph Tan on 23/10/2018.
  */
+
+let axios = require("axios");
+
+
+let axiosInstance = axios.create({
+    baseURL: "/data/"
+});
+
+module.exports = {
+    axiosInstance: axiosInstance
+};
