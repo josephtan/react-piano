be77fc4ac394aed1d4ee55fd430d317982a5c5db
josephtan <tantjernbit@gmail.com>
2018-10-26 14:27:14 +1100

    import new component SplitLettering

--- src/js/App.js
+++ src/js/App.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import "../css/App.scss";
 import PlaySound from "./components/playsound";
 import DrawKeyboard from "./components/drawkeyboard";
+import SplitLettering from"./components/splitlettering";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            <h1>React Piano</h1>
+            <h1><SplitLettering/></h1>
         </header>
         <main className="App-main">
           <DrawKeyboard/>
           <PlaySound/>
         </main>
         <footer className="App-footer">
-            <p>React Piano, keys are drawn with D3.js svg</p>
+            <p className="font-small-size">React Piano, keys are drawn with D3.js svg</p>
         </footer>
       </div>
     );
