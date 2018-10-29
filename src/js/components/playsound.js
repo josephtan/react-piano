3a8c90c62ef83a4ea8c31f895a8a9dfeb7394944
josephtan <tantjernbit@gmail.com>
2018-10-29 19:02:33 +1100

    mapped audio function for white keys, need to add notes according to json data

--- src/js/components/playsound.js
+++ src/js/components/playsound.js
@@ -23,20 +23,28 @@ export default class PlaySound extends Component{
         this.audioList();
     }
     audioList = () => {
-        let audioTag = document.createElement("audio");
-        audioTag.src ="./audio/note1o.mp3";
-        audioTag.type="audio/mpeg";
-           for (let i =0; i < this.noteWhite; i++){
+
+        const audioNotes = this.state.soundata.map((notes) =>{
+            let audioTag = document.createElement("audio");
+            audioTag.setAttribute("id", "audio-" + notes.index);
+            audioTag.type="audio/mpeg";
+            audioTag.src = notes.src;
+            return (audioTag);
+        });
+
+           for (let i=0; i < this.noteWhite; i++){
                /** Somehow the on. is d3's global even listener and will conflict if used in another component in React **/
                d3.select("#note-white-"+i).on("mousedown", function(){
-                   audioTag.play();
                    d3.select("#note-white-"+i).attr("fill","#e3e1d4");
+                   audioNotes[i].play();
+                   console.log(audioNotes[i]);
                });
                d3.select("#note-white-"+i).on("mouseup",function(){
                    d3.select("#note-white-"+i).attr("fill","#fffff0");
-                   audioTag.currentTime=0;
+                   audioNotes[i].currentTime=0;
                });
-            }
+
+           }
        };
     render(){
         return(<div>
